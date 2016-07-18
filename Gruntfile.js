d6363534475349fce0e16cddadaaaf65666f2934
Oskar Gustafsson <oskargustafsson88@gmail.com>
2016-07-18 22:14:32 +0200

    Grunt tasks for compiling JS and CSS

---
+++ Gruntfile.js
@@ -0,0 +1,71 @@
+/* global module */
+module.exports = function (grunt) {
+	'use strict';
+
+	grunt.initConfig({
+
+		jshint: {
+			src: 'src/*.js',
+			options: {
+				jshintrc: true,
+			},
+		},
+
+		jscs: {
+			src: 'src/*.js',
+			options: {
+				config: true,
+			},
+		},
+
+		sass: {
+			options: {
+				sourceMap: true,
+			},
+			compile: {
+				files: {
+					'dist/main.css': 'src/main.scss',
+				},
+			},
+		},
+
+		requirejs: {
+			compile: {
+				options: {
+					mainConfigFile: 'src/main.js',
+					include: [ 'main.js' ],
+					out: 'dist/main.js',
+					optimize: 'uglify',
+				},
+			},
+		},
+
+		watch: {
+			js: {
+				files: 'src/**/*.js',
+				tasks: [ 'requirejs' ],
+				options: {
+					spawn: false,
+				},
+			},
+			sass: {
+				files: 'src/**/*.scss',
+				tasks: [ 'sass' ],
+				options: {
+					spawn: false,
+				},
+			},
+		},
+
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-jscs');
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-requirejs');
+
+	grunt.registerTask('default', [ 'jshint', 'jscs', 'sass', 'requirejs', 'watch' ]);
+
+};
